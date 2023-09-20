d830b7ec53cdd14d103b451dd35df6cde3c5a864
tobias <33268192+tobiasvanderwerff@users.noreply.github.com>
2023-09-20 15:25:17 +0200

    Python implementation

--- attention.py
+++ attention.py
@@ -1,26 +1,32 @@
 import numpy as np
+from timeit import timeit
 
 def attention(Q, K, V):
     """ 
-    Calculate the attention weights.
-
     Args:
-        Q: query matrix, shape (batch_size, seq_len, d_model)
-        K: key matrix, shape (batch_size, seq_len, d_model)
-        V: value matrix, shape (batch_size, seq_len, d_model)
-
-    Returns:
-        weights: attention weights, shape (batch_size, seq_len, seq_len) 
+        Q: query matrix, shape (seq_len, d_model)
+        K: key matrix, shape (seq_len, d_model)
+        V: value matrix, shape (seq_len, d_model)
     """
     QK = Q @ K.T
-    dk = np.sqrt(K.shape[-1])
-    QK = QK / dk
-    weights = np.exp(QK)
-    return weights
+    max_QK = np.amax(QK, axis=-1, keepdims=True)
+    P = np.exp(QK - max_QK)
+    P /= P.sum(axis=-1, keepdims=True)
+    O = P @ V
+
+def benchmark_attention_python(N, d):
+    Q = np.random.rand(N, d)
+    K = np.random.rand(N, d)
+    V = np.random.rand(N, d)
+    usecs = timeit(lambda: attention(Q, K, V), number=2) / 2 * 1000000
+    # gflops = ((2*M*N*K)/secs) / 1e9
+    # print(gflops, "GFLOP/s")
+    return usecs
 
 
 if __name__ == "__main__":
-    Q = np.ones((2, 3))
-    K = V = Q
-    weights = attention(Q, K, V)
-    print(weights)
+    N = 512
+    d = 256
+
+    usecs_python = benchmark_attention_python(N, d)
+    print(usecs_python, "us")
